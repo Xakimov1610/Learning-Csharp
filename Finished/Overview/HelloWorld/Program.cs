f3d1edac08005961a5a59845600c31d9d7c69ff9
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-04-13 16:15:18 +0500

    Name

--- Finished/Overview/HelloWorld/Program.cs
+++ Finished/Overview/HelloWorld/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// The namespace makes it easier to organize our application and 
+// avoid name collisions with other code
+namespace HelloWorld
+{
+    // In .NET our program is contained within its own class
+    class Program
+    {
+        // The main function is the entry point into our app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+
+            // create a string variable and ask the user for some input
+            string str = Console.ReadLine();
+            Console.WriteLine("Why, hello there " + str);
+        }
+    }
+}
